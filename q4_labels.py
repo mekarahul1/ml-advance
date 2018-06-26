9cbb66b3cd841208c3b55c09e228ca3c077772e2
mekarahul1 <33515465+mekarahul1@users.noreply.github.com>
2018-06-26 18:02:42 +0530

    Update q4_labels.py
    add multiple cases

--- q4_labels.py
+++ q4_labels.py
@@ -11,7 +11,28 @@ response = rekognition.detect_labels(
     Image={
         'S3Object': {
             'Bucket': "imagesofme",
-            'Name': "mountains.jpeg",
+            'Name': "mountains.jpeg", # image has a mountain
+        },
+    },
+    MaxLabels=123,
+    MinConfidence=90,
+)
+response2 = rekognition.detect_labels(
+    Image={
+        'S3Object': {
+            'Bucket': "imagesofme",
+            'Name': "group.jpg", # image has  10 people
+        },
+    },
+    MaxLabels=123,
+    MinConfidence=90,
+)
+
+response3 = rekognition.detect_labels(
+    Image={
+        'S3Object': {
+            'Bucket': "imagesofme",
+            'Name': "rahul1.jpg",  # image has a face
         },
     },
     MaxLabels=123,
@@ -21,6 +42,10 @@ response = rekognition.detect_labels(
 #print(response['Labels'])
 k=response['Labels']
 c=0
+c1=0
+c2=0
+k2=response2['Labels']
+k3=response2['Labels']
 for i in range(len(k)):
     if(k[i]['Name']=='Mountain'):
         print("Montain object is present in image")
@@ -28,4 +53,22 @@ for i in range(len(k)):
         break;
         
 if(c==0):
+    print("object not found")
+
+for i in range(len(k2)):
+    if(k2[i]['Name']=='Mountain'):
+        print("for 2nd image Montain object is present in image")
+        c1=c1+1;
+        break;
+        
+if(c1==0):
+    print("object not found")
+
+for i in range(len(k3)):
+    if(k3[i]['Name']=='Mountain'):
+        print("for 2nd image Montain object is present in image")
+        c2=c2+1;
+        break;
+        
+if(c2==0):
     print("object not found")
